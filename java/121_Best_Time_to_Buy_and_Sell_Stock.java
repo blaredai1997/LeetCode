16114bd715062f9170231f2353b22ad99f0241c1
Blare <yc927596@dal.ca>
2022-05-14 16:46:02 -0400

    add q121

---
+++ java/121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE, profit = 0;
+        for (int i=0; i<prices.length; i++) {
+            min = Math.min(prices[i], min);
+            profit = Math.max(prices[i]-min, profit);
+        }
+        return profit;
+    }
+}
