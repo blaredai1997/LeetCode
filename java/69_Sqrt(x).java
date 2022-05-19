a64bd6ebd23816b3cff1a25faf696c97d01a3f8f
Blare <yc927596@dal.ca>
2022-05-19 17:01:24 -0400

    add q69

---
+++ java/69_Sqrt(x).java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l = 1, r = x;
+        while (l<=r) {
+            int mid = (l+r)/2;
+            if (mid>x/mid)
+                r = mid - 1;
+            else 
+                l = mid + 1;
+        }
+        return r;
+    }
+}
