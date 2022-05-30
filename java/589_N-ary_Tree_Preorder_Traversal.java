96c6030db8eded0e6464d0f9567dd455d37a508b
Blare <yc927596@dal.ca>
2022-05-30 14:44:53 -0400

    add q589

---
+++ java/589_N-ary_Tree_Preorder_Traversal.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        recursion(res, root);    
+        return res;
+        
+    }
+    public void recursion(List<Integer> res, Node curr) {
+        if (curr==null) return;
+        res.add(curr.val);
+        for (Node child : curr.children)
+            recursion(res, child);
+    }
+}
