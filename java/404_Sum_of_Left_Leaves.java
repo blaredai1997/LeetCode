626f8898bec7eae267e398b6b7a5e40cae4e82ae
Blare <yc927596@dal.ca>
2022-07-06 20:14:04 -0400

    add q404

---
+++ java/404_Sum_of_Left_Leaves.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        recursion(root.left, true);
+        recursion(root.right, false);
+        return sum;
+    }
+    public void recursion(TreeNode root, boolean isLeft) {
+        if (root==null) return;
+        if (root.left==null && root.right==null && isLeft) sum += root.val;
+        recursion(root.left, true);
+        recursion(root.right, false);
+    }
+}
