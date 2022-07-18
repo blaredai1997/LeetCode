ffde1af90d147d5f1e90ad793d4fde5d3ed8deb3
Blare <yc927596@dal.ca>
2022-07-18 15:35:11 -0400

    add q1822

---
+++ java/1822_Sign_of_the_Product_of_an_Array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int res = 1;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]==0) return 0;
+            if (nums[i]<0) res = -res;
+        }
+        return res;
+    }
+}
